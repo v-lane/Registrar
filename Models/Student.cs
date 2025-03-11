dedf8d4ccfc5c2420844e9e5655bdd733c773a5e
Victoria Lane <lane0085@algonquinlive.com>
2025-03-11 19:27:21 -0400

    Create Student class

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+namespace Registrar;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Type {get; set;}
+    public List<Course> Courses {get; set;}
+    
+}
