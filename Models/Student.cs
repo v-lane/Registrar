0ad39a3fbb704ce62ec8d4509548c874d5ee5f83
Victoria Lane <lane0085@algonquinlive.com>
2025-04-05 13:51:09 -0400

    Add form validation

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,9 +7,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class Student
 {
     [Key] public int Id { get; set; } = new Random().Next(100000, 999999);
-    [Required][MaxLength(50)] public string FirstName { get; set; } = "";
-    [Required][MaxLength(100)] public string LastName { get; set; } = "";
-    [Required][MaxLength(10)] public string Type { get; set; } = "";
+    [Required(ErrorMessage = "First Name Required")][MaxLength(50)] public string FirstName { get; set; } = "";
+    [Required(ErrorMessage = "Last Name Required")][MaxLength(100)] public string LastName { get; set; } = "";
+    [Required(ErrorMessage = "Student Type Required")][MaxLength(10)] public string Type { get; set; } = "";
 
     // Navigation Property
     public ICollection<Registration>? Registrations { get; set; }
