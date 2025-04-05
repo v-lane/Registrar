bc1be7637d536b8d3d26a67587a8758a311b92e4
Victoria Lane <lane0085@algonquinlive.com>
2025-04-05 12:24:35 -0400

    Add form validation and interaction with database to create/delete records

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,19 +1,18 @@
 namespace Registrar.Models;
+
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 [Table("student")]
 public class Student
 {
-    [Key]
-    public int Id { get; set; }
-    public string FirstName { get; set; } = "";
-    public string LastName { get; set; } = "";
-    public string Type {get; set;} = "";
-    
+    [Key] public int Id { get; set; } = new Random().Next(100000, 999999);
+    [Required][MaxLength(50)] public string FirstName { get; set; } = "";
+    [Required][MaxLength(100)] public string LastName { get; set; } = "";
+    [Required][MaxLength(10)] public string Type { get; set; } = "";
+
     // Navigation Property
     public ICollection<Registration>? Registrations { get; set; }
-    
+
     // public List<Course> Courses {get; set;}
-    
 }
